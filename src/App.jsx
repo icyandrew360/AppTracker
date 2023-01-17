17236ff600f544ff43eaffb0545aefd74573e37e
Andrew Howe <andrewhowe1000@gmail.com>
2023-01-16 20:27:22 -0700

    added mainBody to UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header'
+import MainBody from './components/MainBody'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MainBody />
     </div>
   )
 }
---
+++ src/components/MainBody.jsx
@@ -0,0 +1,13 @@
+import "./MainBody.css"
+
+export default function MainBody(){
+    return(
+        <div className="MainBody">
+            <div className="MainBody--Header">
+                <p>Company</p>
+                <p>Date Submitted</p>
+                <p>Status</p>
+            </div>
+        </div>
+    )
+}
